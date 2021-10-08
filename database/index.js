c29f6e58c98c8228cd940cc28e8547fd98b67870
zeenatarshad <zputhawala3011@gmail.com>
2021-10-08 23:58:37 +0530

    first commit

---
+++ database/index.js
@@ -0,0 +1,52 @@
+let books = [
+    {
+        ISBN: "12345ONE",
+        title: "Getting started with MERN",
+        authors: [1,2],
+        language: "en",
+        pubDate: "2021-07-07",
+        numOfPage: 225,
+        category: ["fiction", "programming", "tech", "web dev"],
+        publication: 1,
+    },
+    {
+        ISBN: "12345TWO",
+        title: "Getting started with Python",
+        authors: [1,2],
+        language: "en",
+        pubDate: "2021-07-07",
+        numOfPage: 225,
+        category: ["fiction", "tech", "web dev"],
+        publication: 1,
+    },
+];
+
+const authors = [
+    {
+        id: 1,
+        name: "Zeenat",
+        books: ["12345ONE","12345TWO"],
+
+    },
+    {
+        id: 2,
+        name: "Zayn",
+        books: ["12345ONE"],
+
+    },
+];
+
+const publications = [
+    {
+    id: 1,
+    name: "Chakra",
+    books: ["12345ONE"],
+    },
+    {
+        id: 2,
+        name: "Snowbell Publications",
+        books: [],
+        },
+];
+
+module.exports = {books, authors, publications};
---
+++ requirements.js
@@ -0,0 +1,83 @@
+// Requirements
+
+// We are a company that handles book publications
+
+// Book
+// ISBN, Title, Author[], Language, Pub Date, Num Page, Category[]
+
+// Authors
+// Name, ID, Books[]
+
+// Publications
+// Name, ID, Books[]
+
+// Requirementts
+
+// ------------------------------------------------------------------------------------------------------------------------
+
+// Books
+
+// We need an API
+
+// GET 
+// to get all books                          ***       +
+// to get specific books                     ***       +
+// to get a list of books based on category  ***       +
+// to get a list of books based on author    {TASK}
+
+// POST
+// New Book                                   ***      +
+
+// PUT
+// Update Book Details                        ***    +
+// Update/ Add New Author                     ***    +
+
+// DELETE
+// delete a book                              ***    +
+// delete a author from a book                ***    +
+
+
+// ------------------------------------------------------------------------------------------------------------------------
+
+// Author
+
+// We need an API
+
+// GET
+// to get all authors                         ***      +
+// to get specific author                     {TASK}
+// to get a list of authors based on a book   ***      +
+
+// POST
+// New Author                                 ***      +
+ 
+// PUT
+// update author name using id                {TASK}
+
+// DELETE
+// Delete an author                           {TASK}
+
+
+// ------------------------------------------------------------------------------------------------------------------------
+
+// Publication
+
+// We need an API
+
+// GET
+// to get all publications                            ***
+// to get specific publication                       {TASK} 
+// to get a list of publications based on a book     {TASK}
+
+// POST
+// Add new Publication                               {TASK}
+
+// PUT
+// Update Publication Details using id                {TASK}
+// Update/ Add new book to a publication              ***
+
+// DELETE
+// delete a book from publication                      ***
+// delete a publication                                 {TASK}
+
+// How does the server serves the request ?
